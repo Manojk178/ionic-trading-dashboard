bbee48654c75d6bab188545c7474f22a8280eea9
Manoj Mandava <mailtomanoj19@gmail.com>
2020-09-02 12:30:44 -0500

    Validation Fields

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,18 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
-
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  private todo: FormGroup;
+  constructor( private formBuilder: FormBuilder ) {
+    this.todo = this.formBuilder.group({
+      title: [''],
+      comments: ['', Validators.required],
+      track: ['', Validators.required],
+      inbound: ['', Validators.required]
+    });
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  logForm(){
+    console.log(this.todo.value);
   }
 
 }
